3f457450787cc078389ed39b22a52aa55fb11189
이희연 HeeYeon Lee <up2kid@gmail.com>
2026-06-01 23:17:29 +0900

    feat: Spring Boot 프로젝트 초기 설정 추가

---
+++ src/main/java/com/team/untitle/UntitleServerApplication.java
@@ -0,0 +1,13 @@
+package com.team.untitle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UntitleServerApplication.class, args);
+    }
+
+}
